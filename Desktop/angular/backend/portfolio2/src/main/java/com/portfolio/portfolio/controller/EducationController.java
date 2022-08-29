fa32997b26ac5b3dfd0a44d3f6dfd3d83e34e281
Octavio Whitaker <whitakeroctavio@gmail.com>
2022-08-29 12:30:10 -0300

    delete method added

--- Desktop/angular/backend/portfolio2/src/main/java/com/portfolio/portfolio/controller/EducationController.java
+++ Desktop/angular/backend/portfolio2/src/main/java/com/portfolio/portfolio/controller/EducationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.portfolio.portfolio.service.EducationService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
@@ -32,7 +33,8 @@ public class EducationController {
     public Iterable<Education>list(){
         return educationService.findAll();
     }
-    @PostMapping("/eliminar/{id}")
+    
+    @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<String> deleteById(@PathVariable Long id){
         String resp = educationService.deleteById(id);
         if("OK".equalsIgnoreCase(resp)){
