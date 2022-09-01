080a283f581fb4e2564d44110584149cd13a6197
WhitakerO <100845539+WhitakerO@users.noreply.github.com>
2022-08-31 23:40:00 -0300

    Update data.sql

--- Desktop/angular/backend/portfolio2/src/main/resources/data.sql
+++ Desktop/angular/backend/portfolio2/src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO usuario (email, nombre, nombre_usuario, password) VALUES ("admin@adm
 INSERT INTO rol SET rol_nombre="ROLE_ADMIN";
 INSERT INTO rol SET rol_nombre="ROLE_USER";
 INSERT INTO usuario_rol (usuario_id, rol_id) VALUES (1,1);
+INSERT INTO aboutus (backgroundimage, name, title) VALUES ("https://i.ibb.co/WKzysHK/abstract-template-blue-geometric-diagonal-overlap-layer-on-dark-blue-background-free-vector.jpg", "Edita el nombre", "E inserta el título aquí");
